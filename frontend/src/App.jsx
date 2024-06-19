5ccc799115a5c7be060a8f7e30123cb8fbee8c18
Joshua Biju Korah <joshuabijukorah@gmail.com>
2024-06-19 20:38:08 +0530

    frontend elements done with static data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
 
 const App = () => {
-  return <div className="container">
-    <List />
-    <Chat />
-    <Detail />
-  </div>;
+  const user = true;
+  return (
+    <div className="container">
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
+      <Notification />
+    </div>
+  );
 };
 
 export default App;
