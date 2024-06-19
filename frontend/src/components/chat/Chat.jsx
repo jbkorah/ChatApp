468ab39f3f7f854893129d5c222f6ab53e471b1b
Joshua Biju Korah <joshuabijukorah@gmail.com>
2024-06-19 14:59:44 +0530

    list and emoji picker

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
-import './chat.css'
-
+import React, { useState } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 const Chat = () => {
+  const [open, SetOpen] = useState(false);
+  const [text, setText] = useState("");
+  console.log(text);
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    SetOpen(false);
+  };
   return (
-    <div className='chat'>Chat</div>
-  )
-}
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe </span>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message ..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => SetOpen((prev) => !prev)}
+          />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
