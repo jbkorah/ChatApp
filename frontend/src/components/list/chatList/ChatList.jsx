784495e42642bbdc0e81a80917f5eec93e8d5a9a
Joshua Biju Korah <joshuabijukorah@gmail.com>
2024-06-19 10:04:24 +0530

    basic structure with html elements

---
+++ frontend/src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./chatList.css";
+
+const ChatList = () => {
+  return <div>ChatList</div>;
+};
+
+export default ChatList;
---
+++ frontend/src/components/list/userInfo/Userinfo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./userInfo.css";
+
+const Userinfo = () => {
+  return (
+    <div>Userinfo</div>
+  )
+}
+
+export default Userinfo
