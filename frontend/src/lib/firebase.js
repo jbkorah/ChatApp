73a85c39e38bcc9c34721ffa60970e16efef9081
Joshua Biju Korah <joshuabijukorah@gmail.com>
2024-07-03 21:34:35 +0530

    api key in env file

--- frontend/src/lib/firebase.js
+++ frontend/src/lib/firebase.js
@@ -19,24 +19,22 @@
 // export const db = getFirestore()
 // export const storage = getStorage()
 
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = { 
-  apiKey: "AIzaSyBWr7qUhLZn_0vWbKnvvPSv_6KEfB0-SAo",
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "reactchat-a42fa.firebaseapp.com",
   projectId: "reactchat-a42fa",
   storageBucket: "reactchat-a42fa.appspot.com",
   messagingSenderId: "93203755518",
-  appId: "1:93203755518:web:7fb13743982f0f557410af"
+  appId: "1:93203755518:web:7fb13743982f0f557410af",
 };
-
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth()
-export const db = getFirestore()
-export const storage = getStorage()
+export const auth = getAuth();
+export const db = getFirestore();
+export const storage = getStorage();
